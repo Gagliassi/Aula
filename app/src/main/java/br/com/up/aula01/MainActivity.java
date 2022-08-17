7f97c7da1bf8d13bf665abf9030b15c5243035d3
09593880950 <gagliassijoao@gmail.com>
2022-08-17 20:22:26 -0300

    Termino de projeto dia 17/08/2022.

--- app/src/main/java/br/com/up/aula01/MainActivity.java
+++ app/src/main/java/br/com/up/aula01/MainActivity.java
@@ -7,10 +7,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     private TextView textViewTitle;
     private Button buttonAction;
+    private ArrayList<String> phrases = new ArrayList<>();
 
 
     @Override
@@ -20,12 +24,22 @@ public class MainActivity extends AppCompatActivity {
 
         textViewTitle = findViewById(R.id.text_title);
         buttonAction = findViewById(R.id.button_action);
+        phrases.add("Eu sou o homem que vai se tornar o rei dos piratas!");
+        phrases.add("Eu tenho um sonho, que é ser o rei dos piratas.!");
+        phrases.add("Saanji, quero comida.");
+        phrases.add("Você dormiu 3 dias, 3 dias?\n" +
+                "Quer dizer que perdi 15 refeições");
+        phrases.add("É patético desistir de algo, sem nem mesmo ter tentado.");
+
 
         View.OnClickListener Listener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                textViewTitle.setText("blablabla");
 
+                Random random = new Random();
+                int position = random.nextInt(phrases.size());
+                String phrase = phrases.get(position);
+                textViewTitle.setText(phrase);
             }
         };
 
